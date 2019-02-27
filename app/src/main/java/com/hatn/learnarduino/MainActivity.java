0ae367bb298e87d5b6b23cef1f8a3dcbdcc4a1db
thuyngocha98 <41480357+thuyngocha98@users.noreply.github.com>
2019-02-27 21:40:32 +0700

    fix login

--- app/src/main/java/com/hatn/learnarduino/MainActivity.java
+++ app/src/main/java/com/hatn/learnarduino/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
+import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -72,28 +73,35 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     Button buttonBasic, buttonSensors, buttonLED, buttonMovement;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        mAuth = FirebaseAuth.getInstance();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+        //navigation drawer bar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        // login firebaseUI
+        mAuth = FirebaseAuth.getInstance();
         if(mAuth.getCurrentUser() != null){
-            //setProfile();
-            Toast.makeText(this, "login", Toast.LENGTH_SHORT).show();
+            setProfile();
         }else {
             functionLogin();
         }
-        super.onCreate(savedInstanceState);
 
-
-
-        setContentView(R.layout.activity_main);
-
-        //logOut();
         //Merge content
-
         buttonBasic = findViewById(R.id.btn_basic);
         buttonSensors=findViewById(R.id.btn_sensors);
         buttonLED=findViewById(R.id.btn_LED);
         buttonMovement=findViewById(R.id.btn_movement);
-
 
 
         buttonBasic.setOnClickListener(new View.OnClickListener() {
@@ -128,21 +136,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             }
         });
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
-
-        //findViewById(R.id.btn_log_out).setOnClickListener(this);
 
     }
 
@@ -163,21 +156,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
@@ -207,6 +185,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/developer?id=Cherala+Apps"));
             startActivity(i);
         } else if (id == R.id.nav_logout) {
+            logOut();
             //TODO: log out from here
         }
 
@@ -216,12 +195,23 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
 
+    // set avatar and information user
     public void setProfile(){
+        String name = "unidentified";
+        String email = "";
+        Uri uriImage;
+        String image = "";
+        try {
+            name = mAuth.getCurrentUser().getDisplayName();
+            email = mAuth.getCurrentUser().getEmail();
+            uriImage = mAuth.getCurrentUser().getPhotoUrl();
 
-        String name = mAuth.getCurrentUser().getDisplayName();
-        String email = mAuth.getCurrentUser().getEmail();
-        String image = mAuth.getCurrentUser().getPhotoUrl().toString();
-
+            if(uriImage != null){
+                image = mAuth.getCurrentUser().getPhotoUrl().toString();
+            }
+        }catch (Exception ex){
+            Toast.makeText(this, ex.toString(), Toast.LENGTH_SHORT).show();
+        }
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         View headerView = navigationView.getHeaderView(0);
@@ -230,13 +220,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         TextView navEmail = (TextView) headerView.findViewById(R.id.tv_header_email);
         navEmail.setText(email);
         ImageView navImage = (ImageView) headerView.findViewById(R.id.img_header);
-        if(image != null){
+        if(!TextUtils.isEmpty(image)){
             Picasso.get().load(image).into(navImage);
+        }
+        else {
+            navImage.setImageResource(R.drawable.user_logo);
         }
 
     }
 
-
+    // login with firebaseUI
     public  void functionLogin(){
         // Choose authentication providers
         List<AuthUI.IdpConfig> providers = Arrays.asList(
@@ -273,28 +266,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
     }
 
-//    @Override
-//    public void onClick(View v) {
-//        if(v.getId() == R.id.btn_log_out){
-//            AuthUI.getInstance()
-//                    .signOut(this)
-//                    .addOnCompleteListener(new OnCompleteListener<Void>() {
-//                        public void onComplete(@NonNull Task<Void> task) {
-//                            // ...
-//                            if(mAuth.getCurrentUser() != null)
-//                                Toast.makeText(MainActivity.this, "Fail", Toast.LENGTH_SHORT).show();
-//                            else{
-//                                Toast.makeText(MainActivity.this, "log out success", Toast.LENGTH_SHORT).show();
-//                                functionLogin();
-//
-//                            }
-//
-//                        }
-//                    });
-//
-//        }
-//    }
-
     private void logOut(){
         AuthUI.getInstance()
                     .signOut(this)
@@ -304,23 +275,22 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                             if(mAuth.getCurrentUser() != null)
                                 Toast.makeText(MainActivity.this, "Fail", Toast.LENGTH_SHORT).show();
                             else{
-                                Toast.makeText(MainActivity.this, "log out success", Toast.LENGTH_SHORT).show();
                                 functionLogin();
-
                             }
 
                         }
                     });
     }
 
+    // experience user
     public void setValueExperience(){
             String user_id = mAuth.getCurrentUser().getUid();
             DatabaseReference current_user_id = FirebaseDatabase.getInstance().getReference().child("Users").child(user_id);
             current_user_id.setValue(0);
     }
 
+    // read data user
     public  void readData(){
-
         String user_id = mAuth.getCurrentUser().getUid();
         DatabaseReference current_user_id = FirebaseDatabase.getInstance().getReference().child("Users").child(user_id);
         current_user_id.addValueEventListener(new ValueEventListener() {
