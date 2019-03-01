82d5fcd37d126b4ad85bb492b4a25f223ad8c8f8
dangquang9a <dangquang9a@gmail.com>
2019-03-01 18:58:16 +0700

    zz

--- app/src/main/java/com/hatn/learnarduino/Tol2/Lesson1/Lesson1Quiz.java
+++ app/src/main/java/com/hatn/learnarduino/Tol2/Lesson1/Lesson1Quiz.java
@@ -75,7 +75,7 @@ public class Lesson1Quiz extends AppCompatActivity {
 
                 Log.d(TAG, "Value is: " + value);
 
-                textView.setText(value);
+                //textView.setText(value);
 
             }
 
@@ -92,7 +92,7 @@ public class Lesson1Quiz extends AppCompatActivity {
 
                 Log.d(TAG, "Value is: " + value);
 
-                code=value;
+                //code=value;
 
             }
 
@@ -104,7 +104,7 @@ public class Lesson1Quiz extends AppCompatActivity {
         Tol2_lesson1_Name.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                activity_name = dataSnapshot.getValue().toString();
+                //activity_name = dataSnapshot.getValue().toString();
             }
 
             @Override
@@ -128,7 +128,7 @@ public class Lesson1Quiz extends AppCompatActivity {
 
                 imageBytes = Base64.decode(value, Base64.DEFAULT);
                 Bitmap decodedImage = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
-                imageView1.setImageBitmap(decodedImage);
+                //imageView1.setImageBitmap(decodedImage);
 
 
             }
@@ -152,7 +152,7 @@ public class Lesson1Quiz extends AppCompatActivity {
 
                 imageBytes = Base64.decode(value, Base64.DEFAULT);
                 Bitmap decodedImage = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
-                imageView2.setImageBitmap(decodedImage);
+                //imageView2.setImageBitmap(decodedImage);
 
             }
 
@@ -166,38 +166,31 @@ public class Lesson1Quiz extends AppCompatActivity {
 
 
         fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(!isFABOpen){
-                    showFABMenu();
-                }else{
-                    closeFABMenu();
-                }
-            }
-        });
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                if(!isFABOpen){
+//                    showFABMenu();
+//                }else{
+//                    closeFABMenu();
+//                }
+//            }
+//        });
 
-        fab1 = findViewById(R.id.fab1);
-        fab2 = findViewById(R.id.fab2);
-        cardViewfab1=findViewById(R.id.card_view_fab1);
-        cardViewfab2=findViewById(R.id.card_view_fab2);
+//        fab1 = findViewById(R.id.fab1);
+//        fab2 = findViewById(R.id.fab2);
+//        cardViewfab1=findViewById(R.id.card_view_fab1);
+//        cardViewfab2=findViewById(R.id.card_view_fab2);
 
 
-        fab2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-                showCustomDialog();
-            }
-        });
-
-        fab1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(Lesson1Quiz.this, Lesson1Content.class);
-                startActivity(i);
-            }
-        });
+//        fab1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent i = new Intent(Lesson1Quiz.this, Lesson1Content.class);
+//                startActivity(i);
+//            }
+//        });
 
 
 
@@ -207,85 +200,85 @@ public class Lesson1Quiz extends AppCompatActivity {
 
     }
 
-    private void showCustomDialog() {
-        //before inflating the custom alert dialog layout, we will get the current activity viewgroup
-        ViewGroup viewGroup = findViewById(android.R.id.content);
-
-        //then we will inflate the custom alert dialog xml that we created
-        final View dialogView = LayoutInflater.from(this).inflate(R.layout.dialog_viewcode, viewGroup, false);
-
-        codeview = dialogView.findViewById(R.id.textview_code);
-        codeview.setText(code);
-        codeview.setMovementMethod(new ScrollingMovementMethod());
-
-        //Now we need an AlertDialog.Builder object
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-
-        //setting the view of the builder to our custom view that we already inflated
-        builder.setView(dialogView);
-
-        //finally creating the alert dialog and displaying it
-        final AlertDialog alertDialog = builder.create();
-
-        btnCopy = dialogView.findViewById(R.id.buttonCopy);
-        btnCancel = dialogView.findViewById(R.id.buttonCancel);
-
-        btnCopy.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-                ClipData clip = ClipData.newPlainText("Copied Code", code);
-                clipboard.setPrimaryClip(clip);
-
-                Snackbar snackbar = Snackbar
-                        .make(coordinatorLayout, "Copied successfully ", Snackbar.LENGTH_LONG);
-                snackbar.show();
-
-                closeFABMenu();
-                alertDialog.dismiss();
-            }
-        });
-
-        btnCancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Snackbar snackbar = Snackbar
-                        .make(coordinatorLayout, "Cancelled ", Snackbar.LENGTH_LONG);
-                snackbar.show();
-
-                closeFABMenu();
-                alertDialog.dismiss();
-            }
-        });
-
-
-        alertDialog.show();
-    }
-
-    public void showFABMenu(){
-        isFABOpen=true;
-        fab.setImageResource(R.drawable.cancel);
-        cardViewfab1.setVisibility(View.VISIBLE);
-        cardViewfab2.setVisibility(View.VISIBLE);
-        fab1.animate().translationY(-getResources().getDimension(R.dimen.standard_55));
-        cardViewfab1.animate().translationY(-getResources().getDimension(R.dimen.standard_55));
-        fab2.animate().translationY(-getResources().getDimension(R.dimen.standard_105));
-        cardViewfab2.animate().translationY(-getResources().getDimension(R.dimen.standard_105));
-
-
-
-    }
-
-    public void closeFABMenu(){
-        isFABOpen=false;
-        fab.setImageResource(R.drawable.ic_menu);
-        cardViewfab1.animate().translationY(0);
-        cardViewfab2.animate().translationY(0);
-        cardViewfab1.setVisibility(View.INVISIBLE);
-        cardViewfab2.setVisibility(View.INVISIBLE);
-        fab1.animate().translationY(0);
-        fab2.animate().translationY(0);
-
-    }
+//    private void showCustomDialog() {
+//        //before inflating the custom alert dialog layout, we will get the current activity viewgroup
+//        ViewGroup viewGroup = findViewById(android.R.id.content);
+//
+//        //then we will inflate the custom alert dialog xml that we created
+//        final View dialogView = LayoutInflater.from(this).inflate(R.layout.dialog_viewcode, viewGroup, false);
+//
+//        codeview = dialogView.findViewById(R.id.textview_code);
+//        codeview.setText(code);
+//        codeview.setMovementMethod(new ScrollingMovementMethod());
+//
+//        //Now we need an AlertDialog.Builder object
+//        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//
+//        //setting the view of the builder to our custom view that we already inflated
+//        builder.setView(dialogView);
+//
+//        //finally creating the alert dialog and displaying it
+//        final AlertDialog alertDialog = builder.create();
+//
+//        btnCopy = dialogView.findViewById(R.id.buttonCopy);
+//        btnCancel = dialogView.findViewById(R.id.buttonCancel);
+//
+//        btnCopy.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+//                ClipData clip = ClipData.newPlainText("Copied Code", code);
+//                clipboard.setPrimaryClip(clip);
+//
+//                Snackbar snackbar = Snackbar
+//                        .make(coordinatorLayout, "Copied successfully ", Snackbar.LENGTH_LONG);
+//                snackbar.show();
+//
+//                closeFABMenu();
+//                alertDialog.dismiss();
+//            }
+//        });
+//
+//        btnCancel.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Snackbar snackbar = Snackbar
+//                        .make(coordinatorLayout, "Cancelled ", Snackbar.LENGTH_LONG);
+//                snackbar.show();
+//
+//                closeFABMenu();
+//                alertDialog.dismiss();
+//            }
+//        });
+//
+//
+//        alertDialog.show();
+//    }
+//
+//    public void showFABMenu(){
+//        isFABOpen=true;
+//        fab.setImageResource(R.drawable.cancel);
+//        cardViewfab1.setVisibility(View.VISIBLE);
+//        cardViewfab2.setVisibility(View.VISIBLE);
+//        fab1.animate().translationY(-getResources().getDimension(R.dimen.standard_55));
+//        cardViewfab1.animate().translationY(-getResources().getDimension(R.dimen.standard_55));
+//        fab2.animate().translationY(-getResources().getDimension(R.dimen.standard_105));
+//        cardViewfab2.animate().translationY(-getResources().getDimension(R.dimen.standard_105));
+//
+//
+//
+//    }
+//
+//    public void closeFABMenu(){
+//        isFABOpen=false;
+//        fab.setImageResource(R.drawable.ic_menu);
+//        cardViewfab1.animate().translationY(0);
+//        cardViewfab2.animate().translationY(0);
+//        cardViewfab1.setVisibility(View.INVISIBLE);
+//        cardViewfab2.setVisibility(View.INVISIBLE);
+//        fab1.animate().translationY(0);
+//        fab2.animate().translationY(0);
+//
+//    }
 
 }
