fedc0ef32c28facfb37dac1dd47e509d9a884287
dangquang9a <dangquang9a@gmail.com>
2019-03-03 10:19:14 +0700

    About us fixed

--- app/src/main/java/com/hatn/learnarduino/Aboutus.java
+++ app/src/main/java/com/hatn/learnarduino/Aboutus.java
@@ -1,6 +1,7 @@
 package com.hatn.learnarduino;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,6 +12,12 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.squareup.picasso.Picasso;
 
 public class Aboutus extends AppCompatActivity {
@@ -18,6 +25,7 @@ public class Aboutus extends AppCompatActivity {
     FloatingActionButton fab;
     TextView textViewEmail;
     ImageView imageView;
+//    StorageReference storageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +38,25 @@ public class Aboutus extends AppCompatActivity {
         textViewEmail=findViewById(R.id.textViewEmail);
         imageView=findViewById(R.id.imageView);
 
+
+//        storageRef=FirebaseStorage.getInstance().getReference().child("aboutus.png");
+//        StorageReference img = storageRef.child("aboutus.png");
+
+//        final long ONE_MEGABYTE = 1024 * 1024;
+//
+//        img.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+//            @Override
+//            public void onSuccess(byte[] bytes) {
+//                Glide.with(Aboutus.this)
+//                        .load(storageRef)
+//                        .into(imageView);
+//            }
+//        }).addOnFailureListener(new OnFailureListener() {
+//            @Override
+//            public void onFailure(@NonNull Exception exception) {
+//                // Handle any errors
+//            }
+//        });
 
 
         fab.setOnClickListener(new View.OnClickListener() {
