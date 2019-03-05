fac5a574af64578a54579505f4871cbde21ae50b
dangquang9a <dangquang9a@gmail.com>
2019-03-05 13:30:13 +0700

    Movement: bugging

--- app/src/main/java/com/hatn/learnarduino/Movement.java
+++ app/src/main/java/com/hatn/learnarduino/Movement.java
@@ -1,6 +1,8 @@
 package com.hatn.learnarduino;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -10,11 +12,13 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.hatn.learnarduino.Tol4.Tol4_Lesson_Content;
 
 public class Movement extends AppCompatActivity {
 
@@ -22,6 +26,8 @@ public class Movement extends AppCompatActivity {
     private CardView btnMovement1,btnMovement2,btnMovement3,btnMovement4,btnMovement5,btnMovement6,btnMovement7,btnMovement8;
     ProgressDialog progressDialog;
     int numberTotalContent = 8;
+
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,7 +45,54 @@ public class Movement extends AppCompatActivity {
 //            }
 //        });
 
+        mAuth = FirebaseAuth.getInstance();
+        String user_id = mAuth.getCurrentUser().getUid();
+
         progressDialog=ProgressDialog.show(this,"Loading app data","Please wait for a while",true);
+
+        final DatabaseReference current_user_id = FirebaseDatabase.getInstance().getReference().child("Users").child(user_id);
+        current_user_id.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                Long value = dataSnapshot.getValue(Long.class);
+                int exp=value.intValue();
+
+                int[] CardViewColor_list = {
+                        R.id.tol4_color1 , R.id.tol4_color2, R.id.tol4_color3, R.id.tol4_color4,
+                        R.id.tol4_color5 , R.id.tol4_color6, R.id.tol4_color7, R.id.tol4_color8,
+                };
+                int[] CardView_List = {
+                        R.id.btn_movement1 ,  R.id.btn_movement2 ,  R.id.btn_movement3 ,  R.id.btn_movement4 ,  R.id.btn_movement5 ,
+                        R.id.btn_movement6 ,  R.id.btn_movement7 ,  R.id.btn_movement8 ,
+                };
+
+                for(int i =0; i <8; i++){
+                    CardView temp = findViewById(CardViewColor_list[i]);
+                    CardView Allcard = findViewById(CardView_List[i]);
+                    if(exp >=5){
+                        temp.setCardBackgroundColor(Color.parseColor("#ff669900"));
+                        temp.setClickable(false);
+                        exp -=5;
+                    }
+                    //Allcard.setClickable(false);
+//                    if (exp>=5)
+//                    {
+//                        for (i=0;i<=exp;i+=5)
+//                        {
+//                            CardView expcheck = findViewById(CardView_List[5/i]);
+//                            expcheck.setClickable(true);
+//                        }
+//                    }
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+            }
+        });
 
         // set visibility with number of lesson
         DatabaseReference number1 = FirebaseDatabase.getInstance().getReference().child("Type_of_lesson").child("Tol4").child("Number_of_lesson");
@@ -62,6 +115,10 @@ public class Movement extends AppCompatActivity {
                         R.id.textview_movement1 ,  R.id.textview_movement2 ,  R.id.textview_movement3 ,  R.id.textview_movement4 ,  R.id.textview_movement5 ,
                         R.id.textview_movement6 ,  R.id.textview_movement7 ,  R.id.textview_movement8 ,
                 };
+                int[] CardViewColor_list = {
+                        R.id.tol4_color1 , R.id.tol4_color2, R.id.tol4_color3, R.id.tol4_color4,
+                        R.id.tol4_color5 , R.id.tol4_color6, R.id.tol4_color7, R.id.tol4_color8,
+                };
 
 
                 //set number lesson visibilyty
@@ -81,6 +138,14 @@ public class Movement extends AppCompatActivity {
                     Function function = new Function();
                     function.SetDataIntoObject(dataTemp, textViewTemp);
                 }
+                for(int i = 0; i < value; i++){
+                    CardView temp = findViewById(CardView_List[i]);
+                    CardView tempcolor = findViewById(CardViewColor_list[i]);
+                    boolean hascolor = tempcolor.isClickable();
+                    ButtonLesson(temp,i+1,hascolor);
+                }
+
+
                 progressDialog.dismiss();
             }
             @Override
@@ -90,9 +155,32 @@ public class Movement extends AppCompatActivity {
 
     }
     @Override
+    public void onBackPressed()
+    {
+        super.onBackPressed();
+        startActivity(new Intent(Movement.this, MainActivity.class));
+        finish();
+
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         onBackPressed();
         return true;
+    }
+
+    private void ButtonLesson(CardView button, final int value, final boolean hascolor)
+    {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Movement.this, Tol4_Lesson_Content.class);
+                i.putExtra("LESSONNUMBERINTENT",value);
+                Log.d(TAG, "test onClick: "+value);
+                i.putExtra("HASCOLOR", hascolor);
+                startActivity(i);
+            }
+        });
     }
 
     private void mergeIdCardView(){
